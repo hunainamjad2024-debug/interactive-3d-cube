d78cef706e16cf1f4ca90b881a8b450900213047
hunainamjad2024-debug <hunainamjad2024@gmail.com>
2026-03-23 14:20:37 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from "https://unpkg.com/three@0.160.0/build/three.module.js";
-import { OrbitControls } from "https://unpkg.com/three@0.160.0/examples/jsm/controls/OrbitControls.js";
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/controls/OrbitControls.js?module";
 
 // Scene
 const scene = new THREE.Scene();
